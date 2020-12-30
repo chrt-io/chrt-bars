2499edb38dfbc172c0ea9e7d97dbcd7f19c78259
carlo <littleark@gmail.com>
2020-12-30 11:00:24 +0000

    support secondary axes

--- src/chrtBars/chrtBars.js
+++ src/chrtBars/chrtBars.js
@@ -23,22 +23,38 @@ function chrtBars() {
   this.attr('stroke', DEAULT_LINE_COLOR);
   this.attr('fill', DEAULT_FILL_COLOR);
   this.attr('strokeWidth', DEFAULT_STROKE_WIDTH);
-  this.fields.y0 = 'y0';
+
+  // console.log('BARCHART FIELDS', this.fields)
 
   this.draw = () => {
     const { _margins, scales } = this.parentNode;
+
+    if(isNull(this.fields.x)) {
+      this.fields.x = scales.x[this.scales.x].field;
+    }
+    if(isNull(this.fields.y)) {
+      //console.log('this.scales', this.scales)
+      //console.log('this.parentNode.scales', this.parentNode.scales)
+      this.fields.y = scales.y[this.scales.y].field;
+    }
+    if(isNull(this.fields.y0)) {
+      this.fields.y0 = `${scales.y[this.scales.y].field}0`;
+    }
+
+    const _scaleX = scales.x[this.scales.x];
+    const _scaleY = scales.y[this.scales.y];
     const _data = this._data.length ? this._data : this.parentNode._data;
     if(!isNull(_data)) {
       _barWidth = _data.reduce((acc, d, i, arr) => {
         const next = arr[i + 1];
         if(!isNull(d) && !isNull(d[this.fields.x]) && !isNull(next) && !isNull(next[this.fields.x])) {
-          const x1 = scales['x'](d[this.fields.x]);
-          const x2 = scales['x'](next[this.fields.x]);
+          const x1 = _scaleX(d[this.fields.x]);
+          const x2 = _scaleX(next[this.fields.x]);
           const delta = Math.abs(x2 - x1);
           acc = delta < acc ? delta : acc;
         }
         return acc;
-      }, scales['x'].barwidth);
+      }, _scaleX.barwidth);
       const flooredBarWidth = Math.floor(_barWidth);
       const barWidth = (flooredBarWidth || _barWidth) || 0;
       const _grouped = this._stacked ? this._stacked._grouped : this._grouped || this._grouped;
@@ -58,15 +74,15 @@ function chrtBars() {
           // rect.setAttribute('shape-rendering', 'crispEdges');
           this.g.appendChild(rect);
         }
-        const x = scales['x'](d[this.fields.x]) - _barWidth / 2;
+        const x = _scaleX(d[this.fields.x]) - _barWidth / 2;
         if(isNaN(x)) {
           return;
         }
-        const y = scales['y'](d[this._stacked ? `stacked_${this.fields.y}` : this.fields.y]);
-        // const y0 = scales['y'](0);
-        let y0 = !isNull(d[this.fields.y0]) ? scales['y'](d[this.fields.y0]) : null;
+        const y = _scaleY(d[this._stacked ? `stacked_${this.fields.y}` : this.fields.y]);
+        // const y0 = _scaleY(0);
+        let y0 = !isNull(d[this.fields.y0]) ? _scaleY(d[this.fields.y0]) : null;
         if(isNull(y0)) {
-          y0 = scales['y'].isLog() ? (scales['y'].range[0] - _margins.bottom) : scales['y'](0);
+          y0 = _scaleY.isLog() ? (_scaleY.range[0] - _margins.bottom) : _scaleY(0);
         }
         // console.log('--->', d, y0)
         rect.setAttribute('x', x);
