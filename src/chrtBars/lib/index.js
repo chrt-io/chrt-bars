e6bf98e659e708515ff3b267a9b577a988d62e8f
carlo <littleark@gmail.com>
2021-06-10 07:20:08 +0100

    added opacity methods

--- src/chrtBars/lib/index.js
+++ src/chrtBars/lib/index.js
@@ -1,4 +1,6 @@
 export { default as lineWidth } from './lineWidth';
 export { default as lineColor } from './lineColor';
 export { default as fill } from './fillColor';
+export { default as fillOpacity } from './fillOpacity';
+export { default as strokeOpacity } from './strokeOpacity';
 export { default as width } from './width';
